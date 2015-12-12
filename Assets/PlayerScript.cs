ec028021185849a2757fe6b06dbf233b0bd81acb
Clayton Andrews <clay.j.andrews@gmail.com>
2015-12-12 12:40:02 -0500

    Just organizing the player class and comments. Wish this didn't actually count as a commit.

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -4,26 +4,27 @@ using System.Collections;
 public class PlayerScript : MonoBehaviour
 {
 	//Player Attributes
+	public Camera followCamera;
 	private Rigidbody playerRigidbody;
 	private float playerSpeed = 0.35f;
 	private float rotationSpeed = 1.0f;
-	private float jumpForceMultiplier = 10.0f;
+	public float playerScale = 1.0f;
+	/*Resources are tagged as good and bad (literally, i.e. "Bad Resource") for the purpose of collision detection for the short-term.
+	 *As the player eats new good objects, increase scale by "some" modifier. (For now, we will increase our scale by 1/2 the collided object's scale).
+	 *Bad things decrease scale; when the scale hits a certain threshold the level should be able to end.
+	 */
+
+	//Jump Action Variables
 	private bool jumping = false;
+	private float jumpForceMultiplier = 10.0f;
+
+	//Dash Action Variables
+	private bool dashing = false;
 	private float dashForceMultiplier = 20.0f;
 	private float dashTimer = 0.0f;
 	private float dashCooldownLength = 3.0f;
-	private bool dashing = false;
-	public Camera followCamera;
-
-	/*Resources are tagged as good and bad (literally, i.e. "Bad Resource") for the purpose of collision detection for the short-term.
-	 *Base scale of blob is currently: 1
-	 *As the player eats new good objects, increase scale by "some" modifier. (For now, we will increase our scale by 1/2 the collided object's scale)
-	 *Bad things decrease scale; when the scale hits a certain threshold the level should be able to end.
-	 */
-	public float playerScale = 1.0f;
-
-	//Quick and dirty way to get the data we need from the input manager to move us around.
-	//Check the Input Manager in Edit-> Project Settings-> Input for more.
+	
+	//Player Input Variables; Check the Input Manager found in Edit-> Project Settings-> Input for more.
 	private float current_horizontal_offset = 0.0f;
 	private float current_vertical_offset = 0.0f;
 	private bool jump_down = false;
@@ -32,7 +33,10 @@ public class PlayerScript : MonoBehaviour
 	// Use this for initialization
 	void Start ()
 	{
-		playerRigidbody = this.GetComponent<Rigidbody>();
+		//Snag a reference to the player's Rigidbody
+		playerRigidbody = GetComponent<Rigidbody>();
+
+		//Set Default Player Scale
 		transform.localScale = new Vector3(playerScale , playerScale , playerScale);
 
 		//set up initial follow camera position
@@ -42,44 +46,46 @@ public class PlayerScript : MonoBehaviour
 	// Update is called once per frame; FixedUpdate per physics step
 	void FixedUpdate ()
 	{
+		//Update Input variables
 		current_horizontal_offset = Input.GetAxis("Horizontal");
 		current_vertical_offset = Input.GetAxis("Vertical");
 		jump_down = Input.GetButton("Jump");
 		dash_down = Input.GetButton("Dash");
 
-		//Movement - in a not so great if block
+		//Player Movement - in a not so great if block
 		if (current_vertical_offset == 0.0f)
 		{
-			//do nothing
+			//do nothing; again, this is bad and needs to be changed
 		}
-		else if(current_vertical_offset < 0f) //S or Down when not using joystick
+		else if(current_vertical_offset < 0f) //S or Down when not using a joystick
 		{
 			playerRigidbody.AddForce(-playerRigidbody.transform.forward * playerSpeed, ForceMode.Impulse);
 		}
-		else // less than 0; W or Up when not using joystick
+		else // less than 0; W or Up when not using a joystick
 		{
 			playerRigidbody.AddForce(playerRigidbody.transform.forward * playerSpeed, ForceMode.Impulse);
 		}
 
-		//Rotation
+		//Player Rotation
 		transform.Rotate(new Vector3(0, (current_horizontal_offset * rotationSpeed), 0), Space.World);
+
+		//Camera Rotation
 		followCamera.transform.Rotate(new Vector3(0, (current_horizontal_offset * rotationSpeed), 0), Space.World);
 
 		//Player Action Buttons
 		if(jump_down && !jumping)
 		{
-			Debug.Log("Jumping");
-			playerRigidbody.AddForce(playerRigidbody.transform.up * jumpForceMultiplier, ForceMode.Impulse);
 			jumping = true;
+			playerRigidbody.AddForce(playerRigidbody.transform.up * jumpForceMultiplier, ForceMode.Impulse);
 		}
 
 		if(dash_down && !dashing)
 		{
-			Debug.Log("Dashing");
-			playerRigidbody.AddForce(playerRigidbody.transform.forward * dashForceMultiplier, ForceMode.Impulse);
 			dashing = true;
+			playerRigidbody.AddForce(playerRigidbody.transform.forward * dashForceMultiplier, ForceMode.Impulse);
 		}
 
+		//Update Dash state/timer
 		if(dashing)
 		{
 			dashTimer += Time.deltaTime;
@@ -91,7 +97,6 @@ public class PlayerScript : MonoBehaviour
 		}
 
 		//Camera update code; the 2 and 5 are unfortunately magic numbers based on where we had previously positioned the camera manually to get the view and angle we wanted.
-		Debug.Log("Forward:" + transform.forward);
 		followCamera.transform.position = Vector3.Lerp(followCamera.transform.position,
 														new Vector3(transform.position.x + (transform.forward.x * -5f * playerScale),
 																	transform.position.y + (2f * playerScale),
@@ -103,9 +108,14 @@ public class PlayerScript : MonoBehaviour
 	void OnCollisionEnter(Collision collision)
 	{
 		var collidedObject = collision.gameObject;
+
+		if (jumping && collidedObject.tag == "Terrain")
+		{
+			jumping = false;
+		}
+
 		if (collidedObject.tag == "Bad Resource")
 		{
-			Debug.Log("BAD THING HIT");
 			/* Collided object needs a uniform scale vector, but you can't >= a vector.
 			 * Currently the resource recycling manager will set uniform scales, so we can just check against a single vector value.
 			 * Otherwise resources need a small script attached with a scale attribute.
@@ -113,24 +123,7 @@ public class PlayerScript : MonoBehaviour
 			if (playerScale >= collidedObject.transform.localScale.x)
 			{
 				//Resize the player
-				this.transform.localScale -= (collidedObject.transform.localScale / 2);
-				//Update internal scale variable for win calculation and such
-				playerScale = transform.localScale.x;
-				//Destroy to be replaced with a recycle command via the Resource Manager
-				Destroy(collidedObject);
-			}
-		}
-		if (collidedObject.tag == "Good Resource")
-		{
-			Debug.Log("GOOD THING HIT");
-			/* Collided object needs a uniform scale vector, but you can't >= a vector.
-			 * Currently the resource recycling manager will set uniform scales, so we can just check against a single vector value.
-			 * Otherwise resources need a small script attached with a scale attribute.
-			 */
-			if (playerScale >= collidedObject.transform.localScale.x)
-			{
-				//Resize the player
-				this.transform.localScale += (collidedObject.transform.localScale / 2);
+				transform.localScale -= (collidedObject.transform.localScale / 2);
 				//Update internal scale variable for win calculation and such
 				playerScale = transform.localScale.x;
 				//Destroy to be replaced with a recycle command via the Resource Manager
@@ -138,9 +131,21 @@ public class PlayerScript : MonoBehaviour
 			}
 		}
 
-		if (jumping && collidedObject.tag == "Terrain")
+		if (collidedObject.tag == "Good Resource")
 		{
-			jumping = false;
+			/* Collided object needs a uniform scale vector, but you can't >= a vector.
+			 * Currently the resource recycling manager will set uniform scales, so we can just check against a single vector value.
+			 * Otherwise resources need a small script attached with a scale attribute.
+			 */
+			if (playerScale >= collidedObject.transform.localScale.x)
+			{
+				//Resize the player
+				transform.localScale += (collidedObject.transform.localScale / 2);
+				//Update internal scale variable for win calculation and such
+				playerScale = transform.localScale.x;
+				//Destroy to be replaced with a recycle command via the Resource Manager
+				Destroy(collidedObject);
+			}
 		}
 	}
 }
