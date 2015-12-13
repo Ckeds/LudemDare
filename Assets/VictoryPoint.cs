42ecbfbe07ef5e9230ff6d20352b383a65511a67
Allen Vanderlinde <allen.vanderlinde@gmail.com>
2015-12-13 11:22:18 -0500

    Added VictoryPoint

---
+++ Assets/VictoryPoint.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VictoryPoint : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    // Player reaches victory
+    void OnTriggerEnter(Collider collision)
+    {
+        var collidedObject = collision.gameObject;
+
+        if (collidedObject.tag == "Player")
+        {
+            Debug.Log("VICTORY!");
+        }
+    }
+}
