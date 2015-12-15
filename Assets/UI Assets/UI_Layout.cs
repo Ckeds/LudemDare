afedab0a65dc6c83a428e4f8ff02593d79c53fd7
Clayton Andrews <clay.j.andrews@gmail.com>
2015-12-14 19:39:29 -0500

    Organization pass; moved assets and scenes into respective folders, working on a UI fix as the text doesn't properly display.

---
+++ Assets/UI Assets/UI_Layout.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI_Layout : MonoBehaviour
+{
+	private PlayerScript Player;
+
+
+	private float dash;
+	private float maxDashSpool;
+	private float jump;
+	private float playerMaxJumpSpool;
+	private int score;
+
+	// Use this for initialization
+	void Awake()
+	{
+		Player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerScript>();
+		maxDashSpool = Player.GetMaxDashSpoolTime();
+		playerMaxJumpSpool = Player.GetMaxJumpSpoolTime();
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		score = (int)((Player.playerScale - 1) * 100);
+		jump = (Player.GetJumpTime() / playerMaxJumpSpool) * 100;
+		dash = (Player.GetDashTime() / maxDashSpool) * 100;
+	}
+}
